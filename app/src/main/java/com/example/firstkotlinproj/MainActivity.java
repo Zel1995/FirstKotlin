d3c867e51b72fe50b387fefb1ddd54217ed99b3f
Zel1995 <glocen@mail.ru>
2021-05-23 12:23:48 +0300

    first Commit

---
+++ app/src/main/java/com/example/firstkotlinproj/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.firstkotlinproj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
